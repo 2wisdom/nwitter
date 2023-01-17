181d5922988f04289d9143452b5598063d96a0b0
Lizzie <leejihye7117@gmail.com>
2023-01-17 17:01:09 +0900

    App 폴더이동

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import AppRouter from "./Router";
+
+function App() {
+  return (
+    <div>
+      <AppRouter />
+      <h1>nwitter</h1>
+    </div>
+  );
+}
+
+export default App;
